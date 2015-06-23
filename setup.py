0359abd3078d75f9bed0b424495bc149470eb271
Brian Jenkins <bcjenkin@C15061000.local>
2015-06-23 10:46:30 -0700

    updates

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from distutils.core import setup
 setup(
-  name = 'fredata',
-  packages = ['fredata'],
-  version = '0.1.0a6',
-  description = 'A package for downloading and manipulating data from Federal Reserve Economic Data',
+  name = 'fredpy',
+  packages = ['fredpy'],
+  version = '0.1.0a',
+  description = 'A package for downloading and working with data from Federal Reserve Economic Data',
   author = 'Brian C. Jenkins',
   author_email = 'bcjenkin@uci.edu',
-  url = 'https://github.com/letsgoexploring/redata',
-#   download_url = 'https://github.com/letsgoexploring/fredata/tarball/0.1',
-  keywords = ['fred', 'economics'],
+  url = 'https://github.com/letsgoexploring/fredpy-package',
+#   download_url = 'https://github.com/letsgoexploring/fredpy-package/tarball/0.1',
+  keywords = ['fred', 'economics','data','federal reserve'],
   classifiers = [],
 )
