306658ac0687b2df563bc5401897fba1c3b219ec
Brian Jenkins <bcjenkin@C15061000.local>
2015-06-23 08:34:10 -0700

    update

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'fredata',
   packages = ['fredata'],
-  version = '0.1.0a1',
+  version = '0.1.0a6',
   description = 'A package for downloading and manipulating data from Federal Reserve Economic Data',
   author = 'Brian C. Jenkins',
   author_email = 'bcjenkin@uci.edu',
