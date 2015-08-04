346022e8eceddc7a42e0f5f6491383ae7585e414
Brian Jenkins <bcjenkin@C15061000.local>
2015-08-03 17:32:26 -0700

    updated package so that function toFredSeries works now.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'fredpy',
   packages = ['fredpy'],
-  version = '0.1.3a',
+  version = '0.4.1',
   description = 'A package for downloading and working with data from Federal Reserve Economic Data',
   author = 'Brian C. Jenkins',
   author_email = 'bcjenkin@uci.edu',
