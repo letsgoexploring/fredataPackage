f51bf551af819898650564468321a0248545fa72
Brian Jenkins <bcjenkin@C15061000.local>
2015-11-12 13:43:43 -0800

    updated steup.py with version #

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
   name = 'fredpy',
   packages = ['fredpy'],
-  version = '0.4.1',
+  version = '0.4.2',
   description = 'A package for downloading and working with data from Federal Reserve Economic Data',
   author = 'Brian C. Jenkins',
   author_email = 'bcjenkin@uci.edu',
